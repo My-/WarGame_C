764a1b49a59446cf6c543e689dee12bf9eb9ee4a
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2017-12-06 15:18:25 +0000

    add deal cards function

--- src/main.c
+++ src/main.c
@@ -14,59 +14,38 @@
 
 // http://www.c4learn.com/c-programming/c-initializing-array-of-structure/
 typedef struct Card{
-    char card[4];
+    char card[5];
     int value;
 } Card;
 
-/**
-*   Creates card deck as 2D array. 4 suits(rows) each 13 cards(coloms).
-*/
-void createNewDeck(Card newDeck[4][CARDS_IN_DECK]);
 
-/**
-*   Gives random cards to player.
-*/
-void getCards(Card palyerDeck[CARDS_PER_PLAYER]);
+void createNewDeck(Card newDeck[4][CARDS_IN_DECK]);
+void getRandomCards(Card palyerDeck[CARDS_PER_PLAYER]);
+void dealCards(int players, Card allPlayersCards[MAX_PALYERS][CARDS_PER_PLAYER]);
 
 void main(){
     srand( time(NULL) ); // random seed
 
-    int palyers;
-    printf("Enter number of players 🂱, : ");
-    scanf("%d", &palyers);
+    int players;
+    printf("Enter number of players : ");
+    scanf("%d", &players);
 
+    Card allPlayersCards[MAX_PALYERS][CARDS_PER_PLAYER];
     Card cardsOnDesk[MAX_PALYERS];
 
-
-    Card newDeck[4][CARDS_IN_DECK];
-    createNewDeck(newDeck);
-
-    Card playerDeck[CARDS_PER_PLAYER];
-    getCards(playerDeck);
-
-    Card playersCards[MAX_PALYERS][CARDS_PER_PLAYER];
-
-    // for(int i = 0; i < CARDS_PER_PLAYER; i++){
-    //     printf("[Card: %s = %d] ", playerDeck[i].name, playerDeck[i].value);
-    // }
-
-    // for(int i = 0; i < SUIT; i++){
-    //     printf("\n");
-    //     for(int j = 0; j < CARDS_IN_DECK; j++){
-    //         printf("[Name: %s, Value: %d] ", newDeck[i][j].name, newDeck[i][j].value);
-    //     }
-    // }
+    dealCards(players, allPlayersCards);
 
 
-    int zxc; scanf("%d", &zxc); // Pause
+    int zxc; scanf("%d", &zxc); // Pause terminal window
 }
 
 
 
 /**
-*   Creates card deck as 2D array. 4 suits(rows) each 13 cards(coloms).
+*   Creates new card deck as 2D array. 4 suits(rows) each 13 cards(coloms).
 */
 void createNewDeck(Card newDeck[4][CARDS_IN_DECK]){
+    // hard coded card deck
     Card cards[4][CARDS_IN_DECK] = {
         {
             {"🂲", 2}, {"🂳", 3}, {"🂴", 4}, {"🂵", 5}, {"🂶", 6}, {"🂷", 7}, {"🂸", 8}, {"🂹", 9},
@@ -83,38 +62,57 @@ void createNewDeck(Card newDeck[4][CARDS_IN_DECK]){
         {
             {"🂢", 2}, {"🂣", 3}, {"🂤", 4}, {"🂥", 5}, {"🂦", 6}, {"🂧", 7}, {"🂨", 8}, {"🂩", 9},
             {"🂪", 10}, {"🂫", 11}, {"🂭", 12}, {"🂮", 13}, {"🂡", 14}
-        },
-
-
+        }
     };
 
+    // copy hard coded array to given array
     for(int i = 0; i < 4; i++){
         for(int j = 0; j < CARDS_IN_DECK; j++){
-            newDeck[i][j] = cards[j];
+            newDeck[i][j] = cards[i][j];
         }
     }
 }
 
 /**
-*   Gives random cards to player.
+*   Gives random cards to the given player.
 */
-void getCards(Card playerDeck[CARDS_PER_PLAYER]){
+void getRandomCards(Card playerDeck[CARDS_PER_PLAYER]){
+    // take new deck of cards
     Card newDeck[4][CARDS_IN_DECK];
     createNewDeck(newDeck);
 
-    for(int i = 0; i < CARDS_PER_PLAYER; i++){
+    int i = 0;
+    // pic randomly cards and give it to player.
+    while(i < CARDS_PER_PLAYER){
         int rNum = rand();
         Card randomCard = newDeck[rNum % 4][rNum % CARDS_IN_DECK];
 
-        if( randomCard.value < 0 ){ continue; }
+        if( randomCard.value < 0 ){ continue; } // card is already given
 
-        playerDeck[i] = randomCard;
-        newDeck[rNum % 4][rNum % CARDS_IN_DECK].value = -1;
+        playerDeck[i] = randomCard; // give card to player
+        newDeck[rNum % 4][rNum % CARDS_IN_DECK].value = -1; // mark card in deck as given
 
         #if defined VERBOSE
-        printf("[ %s = %d ] ", playerDeck[i].name, playerDeck[i].value);
+        printf("[ %s = %d ] ", playerDeck[i].card, playerDeck[i].value);
         #endif
+
+        i++;
     }
     printf("\n");
 
+}
+
+/**
+*   gives random cards to the given number of players.
+*/
+void dealCards(int players, Card allPlayersCards[MAX_PALYERS][CARDS_PER_PLAYER]){
+    // give card to players
+    for(int i = 0; i < players; i++){
+        Card playerCards[CARDS_PER_PLAYER];
+        getRandomCards(playerCards);
+        // copy players cards
+        for(int j = 0; j < CARDS_PER_PLAYER; j++){
+            allPlayersCards[i][j] = playerCards[j];
+        }
+    }
 }
