ef7d95ef03bef043ead61a33e6d455e751807547
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2017-12-19 21:21:44 +0000

    clean up

--- src/myUtils.c
+++ src/myUtils.c
@@ -3,3 +3,17 @@
 void clearScreen(){
     for(int i = 0; i < 100; i++){ println(); }
 }
+
+
+/**
+*   runs until user enters space
+*/
+void enterSpace(){
+    char ch;
+    fflush(stdin); // flush buffer
+
+    while ((ch = getchar()) != ' '){
+        printf("\t(Space to continue)\n");
+        fflush(stdin); // flush buffer
+    }
+}
--- src/myUtils.h
+++ src/myUtils.h
@@ -2,7 +2,9 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+#include <stdio.h>
 // the entire header file file
 void clearScreen();
+void enterSpace();
 
 #endif
